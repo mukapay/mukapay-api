07d0df46d54958c0d563b1c1581a6d413bcb6f1c
yikkai <yikkai95@gmail.com>
2025-03-22 01:56:52 +0800

    feat: add pay api

---
+++ api/constant.ts
@@ -0,0 +1,203 @@
+export const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_verifierAddress",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "_usdcAddress",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "usernameHash",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "Deposited",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "fromUsernameHash",
+          "type": "uint256"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "toUsernameHash",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "Paid",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "balances",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "usernameHash",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "deposit",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256[2]",
+          "name": "a",
+          "type": "uint256[2]"
+        },
+        {
+          "internalType": "uint256[2][2]",
+          "name": "b",
+          "type": "uint256[2][2]"
+        },
+        {
+          "internalType": "uint256[2]",
+          "name": "c",
+          "type": "uint256[2]"
+        },
+        {
+          "internalType": "uint256",
+          "name": "fromUsernameHash",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "toUsernameHash",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "nonce",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "resultHash",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "pay",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "usdc",
+      "outputs": [
+        {
+          "internalType": "contract IERC20",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "usedNonces",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "verifier",
+      "outputs": [
+        {
+          "internalType": "contract IVerifier",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ] as const;
--- api/index.ts
+++ api/index.ts
@@ -2,18 +2,24 @@ import 'dotenv/config'
 import { Hono } from 'hono'
 // import { handle } from 'hono/vercel'
 import { serve } from '@hono/node-server'
-import { createPublicClient, http } from 'viem'
-import { baseSepolia, mainnet } from 'viem/chains'
+import { createPublicClient, encodeFunctionData, http } from 'viem'
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts'
+import { baseSepolia } from 'viem/chains'
 import { getUsernameHash } from './util.js'
-export const config = {
-  runtime: 'edge'
-}
+import { abi } from './constant.js'
+import { createBundlerClient, toCoinbaseSmartAccount } from 'viem/account-abstraction'
+
+// export const config = {
+//   runtime: 'edge'
+// }
+
 
 // Initialize viem client
 const client = createPublicClient({
   chain: baseSepolia,
   transport: http(process.env.RPC_URL as string)
 })
+
 
 
 const app = new Hono().basePath('/api')
@@ -30,29 +36,110 @@ app.get('/users/:username/balance', async (c) => {
     const balance = await client.readContract({
       address: process.env.VAULT_ADDRESS as `0x${string}`, // USDC contract
       functionName: 'balances',
-      abi: [
-        {
-          inputs: [{ name: 'account', type: 'uint256' }],
-          name: 'balances',
-          outputs: [{ name: '', type: 'uint256' }],
-          stateMutability: 'view',
-          type: 'function'
-        }
-      ],
+      abi,
       args: [usernameHash]
     })
 
-    return c.json({ 
+    return c.json({
       address: username,
       balance: balance.toString(),
       token: 'USDC'
     })
   } catch (error) {
-    return c.json({ 
+    return c.json({
       error: 'Failed to fetch balance',
       message: error instanceof Error ? error.message : 'Unknown error'
     }, 400)
   }
+})
+
+app.post('/pay', async (c) => {
+  const { payment_proof, from_hash, to_hash, amount } = await c.req.json()
+
+  const paymentFormatted = {
+    pi_a: payment_proof.proof.pi_a.slice(0, 2),
+    pi_b: [
+      [payment_proof.proof.pi_b[0][1], payment_proof.proof.pi_b[0][0]],
+      [payment_proof.proof.pi_b[1][1], payment_proof.proof.pi_b[1][0]]
+    ],
+    pi_c: payment_proof.proof.pi_c.slice(0, 2)
+  };
+
+  const adminPrivateKey = generatePrivateKey()
+  const admin = privateKeyToAccount(adminPrivateKey)
+
+
+  const account = await toCoinbaseSmartAccount({
+    client,
+    owners: [admin],
+  })
+
+  const bundlerClient = createBundlerClient({
+    account,
+    client,
+    transport: http(process.env.RPC_URL as string),
+    chain: baseSepolia
+  })
+
+  console.log(amount)
+
+  const payCall = {
+    to: process.env.VAULT_ADDRESS as `0x${string}`,
+    data: encodeFunctionData({
+      abi,
+      functionName: 'pay',
+      args: [
+        paymentFormatted.pi_a,
+        paymentFormatted.pi_b,
+        paymentFormatted.pi_c,
+        from_hash,
+        to_hash,
+        payment_proof.publicSignals[1],
+        payment_proof.publicSignals[2],
+        amount
+      ],
+    })
+  }
+
+  account.userOperation = {
+    estimateGas: async (userOperation) => {
+      console.log("Estimating gas for user operation:", userOperation)
+      const estimate = await bundlerClient.estimateUserOperationGas(userOperation);
+      console.log("Initial gas estimate:", estimate)
+
+      // Adjust gas limits for complex transactions
+      estimate.preVerificationGas = estimate.preVerificationGas * 2n;
+      return estimate;
+    },
+  };
+
+  try {
+    const userOpHash = await bundlerClient.sendUserOperation({
+      account,
+      calls: [payCall],
+      paymaster: true
+    })
+
+    console.log("UserOperation Hash:", userOpHash)
+
+    const receipt = await bundlerClient.waitForUserOperationReceipt({
+      hash: userOpHash,
+    })
+
+    return c.json({
+      bundler_tx_hash: userOpHash,
+      sender: account.address,
+      tx_hash: receipt.receipt.transactionHash
+    })
+  } catch (error) {
+    console.error("Error sending transaction:", error)
+    return c.json({
+      error: 'Failed to send transaction',
+      message: error instanceof Error ? error.message : 'Unknown error'
+    }, 400)
+  }
+
+
 })
 
 // export default handle(app)
